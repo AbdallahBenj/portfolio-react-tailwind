230869585152c68e262d538ecafd4638e908d9df
AbdallahBenj <abdallahbenjalal@gmail.com>
2026-01-06 01:10:31 +0100

    Updates

--- assets/index-DTot9Axv.js
+++ assets/index-DTot9Axv.js
@@ -46,10 +46,10 @@ Error generating stack: `+u.message+`
         dark:shadow-[2px_4px_12px_3px] dark:shadow-gray-500`,children:R.jsx(Om,{className:`h-full w-full 
           text-gray-700 dark:text-blue-500 
           transition-colors duration-500`})}),R.jsxs("div",{className:`text-container 
-        flex flex-col justify-end mb-5 md:mb-10`,children:[R.jsx("p",{className:`text-xl font-bold font-montserrat 
+        flex flex-col justify-end mb-5 md:mb-10`,children:[R.jsx("p",{className:`text-xl font-bold font-sans
             text-gray-700 dark:text-gray-300
             scale-x-73 origin-left`,children:"Hello, I'm"}),R.jsx("div",{className:`line 
-          border-b-2 w-16 mt-2 mb-12 text-gray-500`}),R.jsx("h2",{className:`font-oswald text-5xl font-bold
+          border-b-2 w-16 mt-2 mb-12 text-gray-500`}),R.jsx("h2",{className:`font-oswald text-5xl font-bold 
             first-letter:text-7xl first-letter:text-gray-800 dark:first-letter:text-blue-500
           text-gray-700 dark:text-gray-300`,children:"MY Name"}),R.jsx("h3",{className:`font-montserrat text-xl 
           text-gray-600 dark:text-gray-300 mt-2`,children:"Front-End Developer"}),R.jsx("div",{className:`line
